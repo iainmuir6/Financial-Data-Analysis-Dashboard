9d248e43530017f254cd561d013fbf0cc01c2df0
Iain Muir <iam9ez@virginia.edu>
2020-11-26 12:44:39 -0500

    Update Stock Market

--- mockPortfolios.py
+++ mockPortfolios.py
@@ -9,6 +9,17 @@ except ModuleNotFoundError:
 from portfolio import Portfolio
 import os
 
+"""
+PORTFOLIO DIVERSIFICATION
+25 Stocks
+    US Stock        (48%)
+        Large-Cap       (7 – 28%)
+        Small-Cap       (5 – 20%)
+    Foreign         (5 – 20%)
+    Indices/ETFs    (5 – 20%)
+    Options         (3 – 12%)
+"""
+
 '''
 PORTFOLIO A
     Crossover Trading Strategy
---
+++ research.py
@@ -0,0 +1,14 @@
+# Iain Muir
+# iam9ez
+
+"""
+PORTFOLIO DIVERSIFICATION
+25 Stocks
+    US Stock        (48%)
+        Large-Cap       (7 – 28%)
+        Small-Cap       (5 – 20%)
+    Foreign         (5 – 20%)
+    Indices/ETFs    (5 – 20%)
+    Options         (3 – 12%)
+"""
+
