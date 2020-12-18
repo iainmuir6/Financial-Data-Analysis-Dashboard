fa55472a9c1a184854d41a12051b8e24f322fb0c
Iain Muir <iam9ez@virginia.edu>
2020-12-18 17:14:40 -0500

    Portfolio Functions

--- home.py
+++ home.py
@@ -3,5 +3,18 @@
 
 import streamlit as st
 
+
 def welcome():
-    print("hi")
+    st.markdown("<center> <img src='https://img.shields.io/badge/python-100%25-brightgreen' "
+                "height='20'/> <img src='https://img.shields.io/badge/implementation-streamlit-orange' "
+                "height='20'/> </center>", unsafe_allow_html=True)
+    # Logos
+    st.markdown("<center>"
+                "<img src='https://upload.wikimedia.org/wikipedia/en/d/da/Robinhood_%28company%29_logo.svg' "
+                "height='50'/>"
+                "<img src='https://upload.wikimedia.org/wikipedia/commons/d/da/Yahoo_Finance_Logo_2019.svg'"
+                "height='125'/> "
+                "</center>", unsafe_allow_html=True)
+    st.write()  # Spacing
+    st.markdown("<h1 style='text-align:center;'> Marketplace Information </h1>", unsafe_allow_html=True)
+    st.write()  # Spacing
--- portfolioDashboard.py
+++ portfolioDashboard.py
@@ -9,50 +9,53 @@ import PIL
 import os
 import io
 
-if os.path.exists("/Users/iainmuir/PycharmProjects/Desktop/stockMarket/portfolioPickles/portfolioB.pickle"):
-    p = pickle.load(open(
-        "/Users/iainmuir/PycharmProjects/Desktop/stockMarket/portfolioPickles/portfolioB.pickle", 'rb'))
 
-    # —————————— Investment Decisions ——————————
-    investments = False
-    # ——————————----------------------——————————
+def portfolio():
+    if os.path.exists("/Users/iainmuir/PycharmProjects/Desktop/stockMarket/portfolioPickles/portfolioB.pickle"):
+        p = pickle.load(open(
+            "/Users/iainmuir/PycharmProjects/Desktop/stockMarket/portfolioPickles/portfolioB.pickle", 'rb'))
 
-    if investments:
-        pickle.dump(p, open(
-            "/Users/iainmuir/PycharmProjects/Desktop/stockMarket/portfolioPickles/portfolioB.pickle", 'wb'))
+        # —————————— Investment Decisions ——————————
+        investments = False
+        # ——————————----------------------——————————
 
-    # Streamlit
+        if investments:
+            pickle.dump(p, open(
+                "/Users/iainmuir/PycharmProjects/Desktop/stockMarket/portfolioPickles/portfolioB.pickle", 'wb'))
 
-    st.markdown("<center> <img src='https://upload.wikimedia.org/wikipedia/en/d/da/Robinhood_%28company%29_logo.svg' "
-                "height='70'/> </center>", unsafe_allow_html=True)
-    st.write(" ")
+        # Streamlit
+        st.markdown("<center> "
+                    "<img src='https://upload.wikimedia.org/wikipedia/en/d/da/Robinhood_%28company%29_logo.svg' "
+                    "height='70'/> "
+                    "</center>", unsafe_allow_html=True)
+        st.write(" ")
 
-    stocks_col, graph_col = st.beta_columns(2)
+        stocks_col, graph_col = st.beta_columns(2)
 
-    stocks_col.subheader("Current Portfolio")
-    for stock in p.stocks.values():
-        logo = stock.logo
-        height = '50'
-        if logo == "":
-            try:
-                url = "https://en.wikipedia.org/wiki/" + stock.company.replace(" ", "_")
-                page = requests.get(url)
-                soup = BeautifulSoup(page.content, "html.parser")
-                logo = "https:" + soup.find("table", class_='infobox vcard').tbody.tr.td.a.img['src']
-                img = PIL.Image.open(io.BytesIO((requests.get(logo)).content))
-                height, width = img.height, img.width
-                height = str(int(width*0.25)) if height > width else str(int(height*0.25))
-            except (Exception, ValueError) as e:
-                # print(e)
-                print("Could not find logo...")
+        stocks_col.subheader("Current Portfolio")
+        for stock in p.stocks.values():
+            logo = stock.logo
+            height = '50'
+            if logo == "":
+                try:
+                    url = "https://en.wikipedia.org/wiki/" + stock.company.replace(" ", "_")
+                    page = requests.get(url)
+                    soup = BeautifulSoup(page.content, "html.parser")
+                    logo = "https:" + soup.find("table", class_='infobox vcard').tbody.tr.td.a.img['src']
+                    img = PIL.Image.open(io.BytesIO((requests.get(logo)).content))
+                    height, width = img.height, img.width
+                    height = str(int(width*0.25)) if height > width else str(int(height*0.25))
+                except (Exception, ValueError) as e:
+                    # print(e)
+                    print("Could not find logo...")
 
-        stocks_col.markdown("<img src='" + logo + "' height='" + height + "' /> " + stock.company + " (" +
-                            stock.ticker + ")  -  $" + str(stock.current_price), unsafe_allow_html=True)
+            stocks_col.markdown("<img src='" + logo + "' height='" + height + "' /> " + stock.company + " (" +
+                                stock.ticker + ")  -  $" + str(stock.current_price), unsafe_allow_html=True)
 
-    graph_col.subheader("Market Value: $" + str(p.market_value))
+        graph_col.subheader("Market Value: $" + str(p.market_value))
 
-else:
-    print("actualPortfolio does not exist...")
-    print("\nHere are the contents of the portfolioPickles folder:")
-    for file in os.listdir("/Users/iainmuir/PycharmProjects/Desktop/stockMarket/portfolioPickles/"):
-        print("\t" + file)
+    else:
+        st.write("actualPortfolio does not exist...")
+        st.write("\nHere are the contents of the portfolioPickles folder:")
+        for file in os.listdir("/Users/iainmuir/PycharmProjects/Desktop/stockMarket/portfolioPickles/"):
+            st.write("\t" + file)
