7ae735678c55549c6e13dc3f02119bd2c6718a82
Iain Muir <iam9ez@virginia.edu>
2020-12-20 11:12:22 -0500

    Home Page

--- home.py
+++ home.py
@@ -14,15 +14,16 @@ def run():
                 "height='20'/> <img src='https://img.shields.io/badge/implementation-streamlit-orange' "
                 "height='20'/> </center>", unsafe_allow_html=True)
     # Logos
-    st.markdown("<center>"
+    st.markdown("<p style='text-align:center;color:white' />"
                 "<img src='https://upload.wikimedia.org/wikipedia/en/d/da/Robinhood_%28company%29_logo.svg' "
-                "height='50'/>"
+                "height='35'/>          -"
                 "<img src='https://upload.wikimedia.org/wikipedia/commons/d/da/Yahoo_Finance_Logo_2019.svg'"
-                "height='125'/> "
+                "height='100'/>         -"
                 "<img src='https://assets.website-files.com/5dc3b47ddc6c0c2a1af74ad0/5e0a328bedb754beb8a973f9_"
-                "logomark_website.png' height='50'/>"
-                "</center>", unsafe_allow_html=True)
-    st.write()  # Spacing
+                "logomark_website.png' height='50'/>            ---"
+                "<img src='https://static.finnhub.io/img/finnhub_2020-05-09_20_51/logo/logo-gradient2.png'"
+                "height='75'"
+                "</p>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align:center;'> Marketplace Information </h1>", unsafe_allow_html=True)
     st.write("\n\n")  # Spacing
     st.markdown(
@@ -44,6 +45,36 @@ def run():
         ```
         $ pip install streamlit
         >>> import streamlit as st
+        ```
+        
+        Market data is sourced from the [Finnhub.io API](https://finnhub.io/). See the [documentation]
+        (https://finnhub.io/docs/api#introduction) for more details on the various endpoints. Endpoints used in this
+        project include
+        * Company Profile – Get general information of a company. You can query by symbol, ISIN or CUSIP.
+        * Market News – Get latest market news.
+        * Company News – List latest company news by symbol.
+         This endpoint is only available for North American companies.
+        * IPO Calendar – Get recent and coming IPO.
+        * Earnings Calendar – Get historical and coming earnings release dating back to 2003.
+         You can setup webhook to receive real-time earnings update.
+        * Quote – Get real-time quote data for US stocks. Constant polling is not recommended.
+         Use websocket if you need real-time update.
+        * Candles – Get candlestick data for stocks going back 25 years for US stocks.
+        * Technical Indicators – Return technical indicator with price data. List of supported indicators
+         can be found [here](https://docs.google.com/spreadsheets/d/1ylUvKHVYN2E87WdwIza8ROaCpd48ggEl1k5i5SgA29k/
+         edit?usp=sharing).
+        
+        Generalized use:
+        
+        ```
+        import requests
+        api_key = "<TOKEN>"
+        response = requests.get('https://finnhub.io/api/v1/<ENDPOINT>?
+                                symbol=<TICKER>&
+                                from=<TIMESTAMP>&
+                                to=<TIMESTAMP>&
+                                token=<API_KEY>
+                   ).json()
         ```
          
         Dashboard is solely programmed and maintained by Iain A. Muir; he is currently a third-year student at the 
