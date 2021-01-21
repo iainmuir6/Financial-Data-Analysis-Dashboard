a2a4b3940337e75ad69fb38bcfad762287c82b05
Iain Muir <iam9ez@virginia.edu>
2021-01-21 18:40:40 -0500

    launch functions

--- app.py
+++ app.py
@@ -17,23 +17,20 @@ https://share.streamlit.io/iainmuir6/stockMarket/master/app.py
 
 import streamlit as st
 
-import home
-import marketNews
-import stockCharts
-import optionChains
-import portfolioDashboard
-import technicalIndicators
+from stockMarket import home, marketNews, stockCharts, optionChains, portfolioDashboard, technicalIndicators
 
-PAGES = {
-    "Home": home,
-    "Market News": marketNews,
-    "Portfolio": portfolioDashboard,
-    "Stock Charts": stockCharts,
-    "Option Chains": optionChains,
-    "Technical Indicators": technicalIndicators
-}
 
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-page = PAGES[selection]
-page.run()
+def launch():
+    PAGES = {
+        "Home": home,
+        "Market News": marketNews,
+        "Portfolio": portfolioDashboard,
+        "Stock Charts": stockCharts,
+        "Option Chains": optionChains,
+        "Technical Indicators": technicalIndicators
+    }
+
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+    page = PAGES[selection]
+    page.run()
